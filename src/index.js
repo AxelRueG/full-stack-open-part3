733208e871069b1b24d177c568a2f3782c3a81ee
Axel Rueda <axelcuer@gmail.com>
2022-02-01 10:57:58 -0300

    exercise 3.8

--- src/index.js
+++ src/index.js
@@ -29,7 +29,19 @@ let db = [
 app.use(express.json());
 
 app.use(
-	morgan(':method :url :status :res[content-length] - :response-time ms')
+	morgan((tokens, req, res) => {
+		const str = JSON.stringify(req.body);
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, 'content-length'),
+			'-',
+			tokens['response-time'](req, res),
+			'ms',
+			str,
+		].join(' ');
+	})
 );
 
 // END POINTS ----------------------------------------------------------
