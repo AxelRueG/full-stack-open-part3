9621ee8149c3004f96e4a1fdef5d5a795ac784bb
Axel Rueda <axelcuer@gmail.com>
2022-02-01 12:11:49 -0300

    refactor 2

--- the-phonebook/src/components/Search.js
+++
@@ -1,9 +0,0 @@
-const Search = ({ search, handleFilter }) => {
-	return (
-		<div>
-			filter shown with <input onChange={handleFilter} value={search} />
-		</div>
-	);
-};
-
-export default Search;
